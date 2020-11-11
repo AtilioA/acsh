69127bfffd8699e8574019883c5744ca3aa51b59
AtilioA <atiliodadalto@hotmail.com>
2020-11-11 10:21:11 -0300

    Cria main.c

---
+++ main.c
@@ -0,0 +1,25 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#include "include/utils.h"
+
+int main(int argc, char *argv[])
+{
+    // if (argc < 3)
+    // {
+    //     return EXIT_FAILURE;
+    // }
+
+    // Guarda nome do arquivo de entrada dos argumentos
+    // char *fileIn = argv[1];
+    // Guarda quantidade de grupos
+    // int k = atoi(argv[2]);
+    // Guarda nome do arquivo de saída
+    // char *fileOut = argv[3];
+
+    printf("Hello, World!\n");
+
+    return EXIT_SUCCESS;
+}
