364fa70400606629eb3d0cda5e7e50736d675fa3
AtilioA <atiliodadalto@hotmail.com>
2020-11-12 09:44:22 -0300

    Cria biblioteca para manipulação de sinais

---
+++ include/signal_handler.h
@@ -0,0 +1,11 @@
+#ifndef __SIGNAL_HANDLER_H
+#define __SIGNAL_HANDLER_H
+
+// Biblioteca com funções para manipulação de sinais
+
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <stdio.h>
+
+#endif
