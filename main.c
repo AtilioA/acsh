7632f1079e6122a12616f51fec7b3205cb547a51
AtilioA <atiliodadalto@hotmail.com>
2020-11-12 10:59:14 -0300

    Faz leitura e checagem de entrada na main

--- main.c
+++ main.c
@@ -1,25 +1,62 @@
-
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
+// #include <unistd.h>
+#include <signal.h>
+#include <string.h>
+// #include <errno.h>
+// #include <sys/time.h>
+// #include <sys/wait.h>
+#include <stdbool.h>
 
 #include "include/utils.h"
+#include "include/string_handler.h"
+#include "include/signal_handler.h"
+#include "include/internal_commands.h"
+#include "include/sys_wrapper.h"
 
-int main(int argc, char *argv[])
+int main(/*int argc, char *argv[]*/)
 {
-    // if (argc < 3)
-    // {
-    //     return EXIT_FAILURE;
-    // }
+    char *input = NULL;
+    char **commands = NULL;
 
-    // Guarda nome do arquivo de entrada dos argumentos
-    // char *fileIn = argv[1];
-    // Guarda quantidade de grupos
-    // int k = atoi(argv[2]);
-    // Guarda nome do arquivo de saída
-    // char *fileOut = argv[3];
+    system("clear");
 
-    printf("Hello, World!\n");
+    /* Handlers de sinais vêm aqui */
+
+    while (true)
+    {
+        printf("acsh> ");
+
+        // Caso ocorra erro na leitura, comece uma nova iteração
+        if (!parse_input(&input))
+        {
+            continue;
+        }
+
+        int isInternal = is_internal_command(input);
+        if (isInternal) // Se for comando interno
+        {
+            run_internal_command(isInternal);
+
+            if (isInternal == EXIT)
+            {
+                free(input);
+                break;
+            }
+        }
+        else // Se for comando externo
+        {
+            // Obtém comandos da string de entrada
+            commands = split_string_token(input, "<3");
+
+            /* Executa ou faz algo com comandos */
+
+            free_commands(commands);
+        }
+
+        // free_commands(commands);
+        free(input);
+    }
 
     return EXIT_SUCCESS;
 }
