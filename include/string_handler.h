97eca7d4f2523f908a0104fa0c316228d3b43f5d
AtilioA <atiliodadalto@hotmail.com>
2020-11-12 10:20:58 -0300

    Adiciona novas funções à string_handler
    Cria parser de comandos ("<3") e trim de strings.

--- include/string_handler.h
+++ include/string_handler.h
@@ -8,6 +8,15 @@
 #include <ctype.h>
 #include <stdio.h>
 
+#define MAX_COMMANDS 5
+
 int parse_input(char **input);
+
+char **split_string_token(char *input, const char *token);
+void free_commands(char **commands);
+
+char *ltrim(char *);
+char *rtrim(char *);
+char *trim(char *);
 
 #endif
--- src/string_handler.c
+++ src/string_handler.c
@@ -22,3 +22,70 @@ int parse_input(char **input)
         return 0;
     }
 }
+
+//quebra a string dada de a acordo com o token
+char **split_string_token(char *input, const char *token)
+{
+    char **stringsArray = malloc(MAX_COMMANDS * sizeof(char *));
+
+    char *str = strtok(input, token); // Divide string em tokens
+
+    for (int i = 0; i < MAX_COMMANDS; i++)
+    {
+        if (!str)
+        {
+            stringsArray[i] = NULL; // Se não houver token, saia
+            break;
+        }
+
+        // Preenche stringsArray com string alocada
+        int tam_str = strlen(str) + 1;
+        stringsArray[i] = malloc(tam_str * sizeof(char));
+        strncpy(stringsArray[i], str, tam_str);
+
+        // Lê próxima string
+        str = strtok(NULL, token);
+    }
+
+    return stringsArray;
+}
+
+// Libera vetor de strings de comandos
+void free_commands(char **commands)
+{
+    for (int i = 0; i < MAX_COMMANDS && commands[i]; i++)
+    {
+        free(commands[i]);
+    }
+    free(commands);
+}
+
+// Remove espaços no início de uma string
+char *ltrim(char *s)
+{
+    while (isspace(*s))
+    {
+        s++;
+    }
+
+    return s;
+}
+
+// Remove espacos no final de uma string
+char *rtrim(char *s)
+{
+    char *back = s + strlen(s); // Aponta para última posição da string
+    while (isspace(*back))
+    {
+        --back; // Retorna posições enquanto existir espaço
+    }
+    *(back + 1) = '\0'; // Coloca '\0' no último espaço encontrado (primeiro)
+
+    return s;
+}
+
+// Remove espaços no início e no final da string
+char *trim(char *s)
+{
+    return rtrim(ltrim(s));
+}
